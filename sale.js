d490396974b2216bc710e9f64118b8e4ad928311
tianming <tianming@xiaokakeji.com>
2015-09-14 17:08:32 +0800

    简化代码

--- sale.js
+++ sale.js
@@ -5,7 +5,7 @@ var Banner = require('./banner');
 var Tabs = require('./tabs');
 var Dimensions = require('Dimensions');
 var {
-  width, 
+  width,
   height
 } = Dimensions.get('window');
 
@@ -18,152 +18,6 @@ var {
   View,
   ActivityIndicatorIOS
 } = React;
-
-var MockData = [
-{
-  "nowPrice": "68.00",
-  "title": "字母贴花蕾丝镂空连衣裙两件套",
-  "cfav": 13896,
-  "img": "http://s17.mogucdn.com/b7/pic/150425/1evair_ieytcolgmy2tgnddgazdambqmeyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "38.85",
-  "title": "松紧腰9分西装哈伦裤",
-  "cfav": 79098,
-  "img": "http://s18.mogucdn.com/b7/pic/150416/18ilnj_ieywknldgq2tgnjzgazdambqmeyde_936x1404.jpg_320x999.jpg",
-  "width": 320,
-  "height": 680
-},
-{
-  "nowPrice": "45.15",
-  "title": "时尚荷叶边A摆两件套装",
-  "cfav": 9661,
-  "img": "http://s17.mogucdn.com/b7/pic/150619/12hykb_ie2wcolega3wimtfgezdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 280
-},
-{
-  "nowPrice": "108.50",
-  "title": "邵夫人2穿迷彩三叶草V领T恤裙",
-  "cfav": 122,
-  "img": "http://s21.mogucdn.com/b7/pic/150626/12dtoj_iezgkn3dmuywgnzqgizdambqmmyde_680x1020.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "38.01",
-  "title": "少女系甜美清新两件套雪纺娃娃衫",
-  "cfav": 10046,
-  "img": "http://s18.mogucdn.com/b7/pic/150620/16fq2q_ie2timdgg4ywiodfgezdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "59.50",
-  "title": "▲夏季新品  百搭蕾丝开衫",
-  "cfav": 2568,
-  "img": "http://s18.mogucdn.com/b7/pic/150609/12hykb_iezweyldmrsgezdbgezdambqgyyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "59.50",
-  "title": "清新甜美水墨印花雪纺连衣裙",
-  "cfav": 459,
-  "img": "http://s21.mogucdn.com/b7/pic/150628/11tbjd_ie2wgntcg5qwmzrqgizdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "49.70",
-  "title": "【KAMI甜蜜爱】3M反光情侣T恤",
-  "cfav": 29,
-  "img": "http://s8.mogucdn.com/p1/150628/14dhcd_ieywcmjtha2damrrgizdambqmeyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "59.85",
-  "title": "【小V家】纯色甜美露肩连衣裙",
-  "cfav": 1337,
-  "img": "http://s18.mogucdn.com/b7/pic/150608/1n09nx_iezgkzbugizdkylbgezdambqmmyde_1000x1500.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "59.50",
-  "title": "【KAMI甜蜜爱】复古情侣衬衫",
-  "cfav": 5935,
-  "img": "http://s18.mogucdn.com/b7/pic/150523/pufv3_ie2wen3dhe4wenjvgezdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "66.67",
-  "title": "邵夫人朋克风大破洞牛仔9分裤",
-  "cfav": 1023,
-  "img": "http://s20.mogucdn.com/b7/pic/150603/12dtoj_iezdoy3ghe4gcyzygezdambqmmyde_1200x1800.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "65.80",
-  "title": "【森林小红帽】夏季细条纹九分裤",
-  "cfav": 29538,
-  "img": "http://s17.mogucdn.com/b7/pic/150328/nyyqg_iezginrshfrwmnjtgazdambqmmyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "39.50",
-  "title": "韩国时尚 抽绳压皱雪纺哈伦裤",
-  "cfav": 446,
-  "img": "http://s17.mogucdn.com/b7/pic/150614/1fra9s_ie2tmyzugqydmoddgezdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "82.60",
-  "title": "【自由人】小香风波点套装背带裙",
-  "cfav": 4704,
-  "img": "http://s18.mogucdn.com/b7/pic/150529/clr96_ieztqylbgnrtqnjxgezdambqgyyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "156.00",
-  "title": "DIY清洁神器美容剂打泡器",
-  "cfav": 1965,
-  "img": "http://s8.mogucdn.com/p1/150630/14dhcd_ie2tiylegi3wgojrgizdambqhayde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "156.00",
-  "title": "清洁神器美容剂打泡器",
-  "cfav": 1965,
-  "img": "http://s18.mogucdn.com/b7/pic/150514/1lpbvr_ieztknrumy4weobsgezdambqgyyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "156.00",
-  "title": "清洁神器美容剂打泡器",
-  "cfav": 1965,
-  "img": "http://s17.mogucdn.com/b7/pic/150612/1o3pe2_iezwmmlbgmydkzlcgezdambqgyyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-},
-{
-  "nowPrice": "86.00",
-  "title": "清洁神器美容剂打泡器",
-  "cfav": 1965,
-  "img": "http://s18.mogucdn.com/b7/pic/150609/1o3pe2_ie2dazrxgu2tkzdbgezdambqgiyde_640x960.jpg_320x999.jpg",
-  "width": 320,
-  "height": 480
-}];
 
 var ajaxUrl = "http://www.mogujie.com/act/mce/get_wall";
 
@@ -239,7 +93,7 @@ var GridList = React.createClass({
     let state = this.state
     let props = this.props
 
-    var style = state.column>1 ? styles.column : {paddingVertical: 5};
+    var style = state.column > 1 ? styles.column : {paddingVertical: 5};
 
     if(!state.loaded){
       return(
@@ -252,43 +106,19 @@ var GridList = React.createClass({
       <ListView contentContainerStyle={[style, props.style,{marginTop: -20}]}
         dataSource={state.dataSource}
         showsVerticalScrollIndicator={false}
-        removeClippedSubviews={true}            //If I use this property, when I scroll listView to the bottom,
-                                                //and then scroll back to the top、switch category, image of listView doesn't update.
-
-        stickyHeaderIndices={[1]}               //It doesn't work
-        onEndReached={this._onEndReached}
         renderHeader={this._renderHeader}
-        renderFooter={this._renderFooter}
         renderRow={this._renderRow}>
       </ListView>
     );
   },
 
-  _onEndReached() {
-    //加载更多数据，追加MockData到之前的cacheData上
-    var data = this.cacheData.concat(MockData);
 
-    this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(data),
-        loaded: true
-    });
-  },
 
   _renderHeader() {
     return (
       <View style={{width:width,marginTop:-10}}>
         <Banner/>
         <Tabs onChange={this.handleUpdateList}/>
-      </View>
-    );
-  },
-
-  _renderFooter() {
-    return (
-      <View style={{width:width,marginVertical:10}}>
-          <View style={[styles.moreBox, styles.centering]}>
-              <Text>查看更多...</Text>
-          </View>
       </View>
     );
   },
@@ -334,7 +164,7 @@ var styles = StyleSheet.create({
   },
 
   goodsInfoBoxView: {
-    
+
     overflow: 'hidden'
   },
 
