a2625b6d2cfa234f37b2ce1255a7d07dad5dad83
tianming <tianming@xiaokakeji.com>
2015-09-14 16:13:22 +0800

    add js file

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var AwesomeProject = require('./sale');
+
+var {
+  AppRegistry
+} = React;
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
